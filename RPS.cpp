b73032b8415a70ca1d3c2e77789b8a3b735dcd7d
KP12p <104709538+KP12p@users.noreply.github.com>
2026-02-24 10:50:48 -0600

    Update RPS.cpp
    You need to change the chor in the beginning to char

--- RPS.cpp
+++ RPS.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 #include <ctime> 
 
-chor getUserChoice();
+char getUserChoice();
 char getComputerChoice();
 void showChoice(char choice);
 void chooseWinner(char player, char computer);
@@ -106,6 +106,7 @@ void chooseWinner(char player, char computer){
     }
    
     }
+
 
 
 
